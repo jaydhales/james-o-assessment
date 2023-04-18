8cd1eb0bedb19855593fa144c113a531f208a21c
jaydhales <69529548+jaydhales@users.noreply.github.com>
2023-04-18 12:28:08 +0100

    Feat: Initialized Project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js";
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return <div class={styles.App}>Starting</div>;
+};
+
+export default App;
