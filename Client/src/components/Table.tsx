8e19bf7bf51d7ba9ebe61addca5135bd1726ed5d
jaydhales <69529548+jaydhales@users.noreply.github.com>
2023-04-19 17:26:52 +0100

    Connected Frontend to Backend

--- Client/src/components/Table.tsx
+++ Client/src/components/Table.tsx
@@ -1,175 +1,58 @@
 import styles from "../App.module.css";
-import { For, Index, createMemo, createSignal } from "solid-js";
+import {
+  For,
+  Index,
+  JSX,
+  createEffect,
+  createMemo,
+  createSignal,
+  onCleanup,
+  onMount,
+} from "solid-js";
 
 type studentData = {
-  _studentId: number;
+  _studentId: string;
   _name: string;
   subjects: subjectData[];
 };
 
 type subjectData = {
-  _subjectId: number;
+  _subjectId: string;
   _name: string;
   score?: number;
 };
 
-const tableData: studentData[] = [
-  {
-    _studentId: 1,
-    _name: "John Doe",
-    subjects: [
-      {
-        _subjectId: 1,
-        _name: "Maths",
-        score: 90,
-      },
-      {
-        _subjectId: 2,
-        _name: "English",
-        score: 95,
-      },
-    ],
-  },
-  {
-    _studentId: 2,
-    _name: "Jane Smith",
-    subjects: [
-      {
-        _subjectId: 1,
-        _name: "Maths",
-        score: 85,
-      },
-      {
-        _subjectId: 2,
-        _name: "English",
-        score: 95,
-      },
-      {
-        _subjectId: 3,
-        _name: "Science",
-        score: 80,
-      },
-      {
-        _subjectId: 4,
-        _name: "History",
-        score: 92,
-      },
-      {
-        _subjectId: 5,
-        _name: "Geography",
-        score: 88,
-      },
-    ],
-  },
-  {
-    _studentId: 3,
-    _name: "Bob Johnson",
-    subjects: [
-      {
-        _subjectId: 1,
-        _name: "Maths",
-        score: 75,
-      },
-      {
-        _subjectId: 2,
-        _name: "English",
-        score: 80,
-      },
-      {
-        _subjectId: 3,
-        _name: "Science",
-        score: 85,
-      },
-    ],
-  },
-  {
-    _studentId: 4,
-    _name: "Alice Williams",
-    subjects: [
-      {
-        _subjectId: 1,
-        _name: "Maths",
-        score: 90,
-      },
-      {
-        _subjectId: 2,
-        _name: "English",
-        score: 92,
-      },
-      {
-        _subjectId: 3,
-        _name: "Science",
-        score: 88,
-      },
-      {
-        _subjectId: 4,
-        _name: "History",
-        score: 85,
-      },
-      {
-        _subjectId: 5,
-        _name: "Geography",
-        score: 80,
-      },
-    ],
-  },
-  {
-    _studentId: 5,
-    _name: "Tom Davis",
-    subjects: [
-      {
-        _subjectId: 1,
-        _name: "Maths",
-        score: 80,
-      },
-      {
-        _subjectId: 2,
-        _name: "English",
-        score: 85,
-      },
-      {
-        _subjectId: 3,
-        _name: "Science",
-        score: 90,
-      },
-      {
-        _subjectId: 4,
-        _name: "History",
-        score: 80,
-      },
-      {
-        _subjectId: 5,
-        _name: "Geography",
-        score: 82,
-      },
-    ],
-  },
-];
-
 const Table = () => {
-  const [activeCol, setActiveCol] = createSignal(1);
+  const [activeCol, setActiveCol] = createSignal("");
+  const [subjects, setSubjects] = createSignal<subjectData[]>([]);
+  const [tableData, setTableData] = createSignal<studentData[]>([]);
+  let mainRef;
 
-  const allSubjects = createMemo(() => {
-    const subjectArr: string[] = [];
+  // createEffect(() => {
+  //   function handleClick(ev: Event) {
+  //     if (mainRef && mainRef.contains(this)) {
+  //       console.log(this);
+  //     }
 
-    tableData.forEach(({ subjects }) => {
-      subjects.forEach(({ _name, _subjectId }) => {
-        const newData = { _name, _subjectId };
-        subjectArr.push(JSON.stringify(newData));
-      });
-    });
+  //     console.log(this);
+  //   }
+  //   document.body.addEventListener("click", handleClick);
 
-    const filteredSub = new Set(subjectArr);
-    const final = Array.from(filteredSub).map((data) => JSON.parse(data));
+  //   onCleanup(() => document.body.removeEventListener("click", handleClick));
+  // });
 
-    return final;
+  onMount(async () => {
+    const res = await fetch("http://localhost:5000/api/subjects");
+    const gradeRes = await fetch("http://localhost:5000/api/grades");
+
+    setSubjects(await res.json());
+    setTableData(await gradeRes.json());
   });
 
-  const getScore = (student: studentData, subjectID: number) => {
+  const getScore = (student: studentData, subjectID: string) => {
     const currScore = student.subjects.filter(
       (sub) => sub._subjectId === subjectID
     );
-
     return currScore.length === 0 ? "-" : currScore[0].score;
   };
 
@@ -177,11 +60,11 @@ const Table = () => {
     <div class={styles.table}>
       <div class={styles.tableCol}>
         <h4></h4>
-        <For each={tableData}>{(student) => <p>{student._name}</p>}</For>
+        <For each={tableData()}>{(student) => <p>{student._name}</p>}</For>
       </div>
 
-      <div class={styles.tableMain}>
-        <For each={allSubjects()}>
+      <div class={styles.tableMain} ref={mainRef}>
+        <For each={subjects()}>
           {(subject) => (
             <div
               class={`${styles.tableCol} ${
@@ -191,7 +74,7 @@ const Table = () => {
             >
               <h4>{subject._name}</h4>
               {
-                <For each={tableData}>
+                <For each={tableData()}>
                   {(student) => <p>{getScore(student, subject._subjectId)}</p>}
                 </For>
               }
--- Server/index.ts
+++ Server/index.ts
@@ -2,11 +2,13 @@ import express, { Express, Request, Response } from "express";
 import gradeRoute from "./routes/gradeRoute";
 import subjectRoute from "./routes/subjectRoute";
 import studentRoute from "./routes/studentRoute";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use("/api/grades", gradeRoute);
 app.use("/api/subjects", subjectRoute);
 app.use("/api/student/:id", studentRoute);
 
-app.listen(3000);
+app.listen(5000);
